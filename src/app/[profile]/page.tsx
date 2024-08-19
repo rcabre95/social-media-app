46593363450374a5f9fed7722f9c617db6190887
Raphael Cabrera <raphaelcabrera@Raphael-Cabreras-MacBook-Pro.local>
2024-08-18 23:24:26 -0500

    updated todos

--- src/app/[profile]/page.tsx
+++ src/app/[profile]/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export default function Profile() {
+export default function Profile({ params }: { params: { profile: string } } ) {
+
+  // TODO: get profile using subdirectory 
+  // TODO: make edit button visible only when session user id = retrieved profile user id
 
   return (
     <div>Profile</div>
   )
 }
-
-// TODO: profile id starts as user id? or we just handle profile id in the intro flow
