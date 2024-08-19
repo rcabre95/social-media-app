93928d874604716b9bc6aaef16dbf941746ea3a3
Raphael Cabrera <raphaelcabrera@Raphael-Cabreras-MacBook-Pro.local>
2024-08-18 23:27:59 -0500

    changed signin to signup

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import SignIn from '@/components/auth/Signin'
+import SignIn from '@/components/auth/SignUp'
+import SignUp from '@/components/auth/SignUp'
 
 export default function Login() {
   return (
     <div>
-      <SignIn />
+      <SignUp />
     </div>
   )
 }
