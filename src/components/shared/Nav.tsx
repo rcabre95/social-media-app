d7b2be5721431c5a372e4c03061501c4e189003b
Raphael Cabrera <raphaelcabrera@Raphael-Cabreras-MacBook-Pro.local>
2024-08-18 23:29:50 -0500

    added cookie logic to nav

--- src/components/shared/Nav.tsx
+++ src/components/shared/Nav.tsx
@@ -4,11 +4,13 @@ import SecondaryBtn from './SecondaryBtn';
 import { createClient } from '@/utils/supabase/client';
 import { useRouter } from 'next/navigation';
 import toast from 'react-hot-toast';
+import { useCookies } from 'react-cookie';
 
 // TODO: Loosely model after linkedin UI/UX
 export default function Nav({ loggedIn }: { loggedIn: boolean }) {
   const supabase = createClient();
   const router = useRouter();
+  const [cookies, setCookie, removeCookie] = useCookies(['hasProfile']);
 
   const logOut = async () => {
 
@@ -17,7 +19,7 @@ export default function Nav({ loggedIn }: { loggedIn: boolean }) {
       toast.error("Something went wrong.")
       return;
     }
-    
+    removeCookie('hasProfile');
     router.push("/");
   }
 
