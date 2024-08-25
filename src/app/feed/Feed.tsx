5a1d0ca00f5638220253e69c023f8c3f7c178a1f
Raphael Cabrera <raphaelcabrera@Raphael-Cabreras-MacBook-Pro.local>
2024-08-25 18:35:22 -0500

    added new post and new project ui. need to finish post functionality

--- src/app/feed/Feed.tsx
+++ src/app/feed/Feed.tsx
@@ -1,8 +1,20 @@
 "use client";
 import { useState, useEffect } from 'react';
 import Nav from '@/components/shared/Nav';
+import NewProjectForm from '@/components/shared/new-project/NewProjectForm';
+import NewPostForm from '@/components/shared/new-post/NewPostForm';
 
 export default function Feed({ profile, onLoad, name }: { profile: any, onLoad?: any, name?: string }) {
+  const [showNewProjectForm, setShowNewProjectForm] = useState<boolean>(false);
+  const [showNewPostForm, setShowNewPostForm] = useState<boolean>(false);
+
+  const toggleNewProject = () => {
+    setShowNewProjectForm(!showNewProjectForm)
+  }
+
+  const toggleNewPost = () => {
+    setShowNewPostForm(!showNewPostForm)
+  }
 
   useEffect(() => {
     if (onLoad && name) {
@@ -11,10 +23,14 @@ export default function Feed({ profile, onLoad, name }: { profile: any, onLoad?:
     }
   }, [])
 
-  return (
-    <>
+  return ( // TODO: figure out where to put Nav
+    <main>
     <Nav loggedIn={true}/>
     <div>Feed</div>
-    </>
+    <button onClick={toggleNewPost} type="button">{!showNewPostForm ? "New Post" : "Cancel"}</button>
+    <button onClick={toggleNewProject} type="button">{!showNewProjectForm ? "New Project" : "Cancel"}</button>
+    {showNewProjectForm ? <NewProjectForm profileId={profile.id} setShowNewProjectForm={setShowNewProjectForm} /> : null}
+    {showNewPostForm ? <NewPostForm profileId={profile.id} setShowNewPostForm={setShowNewPostForm} /> : null}
+    </main>
   )
 }
